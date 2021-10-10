4edb50803ac4c59daa2b0567363311134b030e9b
khairellah <zakariakhairellah@gmail.com>
2021-10-10 08:16:04 +0200

    add js code for gif loading in slow network

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
+/* Get HTML ELEMENTS */
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
 
+/* ADD EVENT FOR PAUSE AND PLAY BTN*/
 btn.addEventListener("click", () => {
   if (!btn.classList.contains("slide")) {
     btn.classList.add("slide");
@@ -13,4 +15,10 @@ btn.addEventListener("click", () => {
     btn.classList.remove("slide");
     video.play();
   }
+});
+
+/*EVENT FOR PRELOADER*/
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", () => {
+  preloader.classList.add("hide-preloader");
 });
